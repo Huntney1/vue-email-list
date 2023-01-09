ec86e3e296cbcd714d2ae6f0c8ad1c14fe000bc0
Huntney1 <devilwhiteone@hotmail.it>
2023-01-10 00:50:06 +0100

    da rivedere non funzionante

--- js/script.js
+++ js/script.js
@@ -3,7 +3,20 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            emails: [],
+            email:'',
         }
     },
-}).mount('#app')
+    mounted() {
+        /* api + risposta con ciclo for */
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((result) => {
+                    this.email = result.data.response;
+                    this.emails.push(this.email); 
+
+                });
+        }
+    }
+
+}).mount('#app');
