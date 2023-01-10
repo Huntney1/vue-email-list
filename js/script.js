0d777dedda5ae069d67323db2b64d5aba4ba2000
Huntney1 <devilwhiteone@hotmail.it>
2023-01-10 15:48:18 +0100

    fix script

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ createApp({
         for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((result) => {
-                    this.mails.push = (result.data.response)
+                    this.mails.push(result.data.response)
                 })
         }
     }
